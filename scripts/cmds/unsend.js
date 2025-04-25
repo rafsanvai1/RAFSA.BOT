d3dd63bed0dbcd5a7139e6a53136d51a85794d64
Robiulislame <robiulislamsohid2@gmail.com>
2025-04-25 17:19:49 +0600

    Update unsend.js

--- scripts/cmds/unsend.js
+++ scripts/cmds/unsend.js
@@ -1,10 +1,12 @@
+const { GoatWrapper } =
+require("fca-liane-utils");
 module.exports = {
 	config: {
-		name: "unsend",
+		name: "uns",
 		version: "1.2",
 		author: "NTKhang",
 		countDown: 5,
-		role: 0,
+		role: 2,
 		description: {
 			vi: "Gỡ tin nhắn của bot",
 			en: "Unsend bot's message"
@@ -15,7 +17,6 @@ module.exports = {
 			en: "reply the message you want to unsend and call the command {pn}"
 		}
 	},
-
 	langs: {
 		vi: {
 			syntaxError: "Vui lòng reply tin nhắn muốn gỡ của bot"
@@ -24,10 +25,11 @@ module.exports = {
 			syntaxError: "Please reply the message you want to unsend"
 		}
 	},
-
 	onStart: async function ({ message, event, api, getLang }) {
-		if (!event.messageReply || event.messageReply.senderID != api.getCurrentUserID())
-			return message.reply(getLang("syntaxError"));
-		message.unsend(event.messageReply.messageID);
-	}
+if (!event.messageReply || event.messageReply.senderl D != api.getCurrentUserID())
+return
+message.reply(getLang("synt axError"));
+message.unsend(event.messageReply.messageID); }
 };
+const wrapper = new GoatWrapper(module.export s);
+wrapper.applyNoPrefix({ allowPrefix: true });
