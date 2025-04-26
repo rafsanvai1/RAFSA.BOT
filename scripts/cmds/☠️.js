1082992fde6fccf9fd147976c35c8e4bc2675527
Robiulislame <robiulislamsohid2@gmail.com>
2025-04-26 10:48:22 +0600

    Create ☠️.js

---
+++ scripts/cmds/☠️.js
@@ -0,0 +1,22 @@
+ module.exports = {
+ config: {
+	 name: "☠️",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "☠️") {
+ return message.reply({
+ body: " 「 🌼🥰༅༎༅আমার༅༎༅ জীবনের༅༎༅ প্রতিটা ༅༎༅গল্পে ༅༎༅আমিই ༅༎༅দোষী༅༎༅😌🌼\n🌻༅༎༅তাই তো༅༎༅ আমি ༅༎༅আনন্দিত༅༎༅ না ༅༎༅হয়েও༅༎༅🌺\n🌻💖༅༎༅বরাবর༅༎༅আমি༅༎༅হাসিখুশি༅༎༅ 🥰🌼\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n🆁🅾🅱🅸🆄🅻」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/5lH0U7H.mp4",)
+ });
+ }
+ }
+ }
