c14cf09d192d6c9c9cf03b23b2021b5b9ff52c91
Robiulislame <robiulislamsohid2@gmail.com>
2025-07-08 17:40:47 +0600

    Create robiul.js

---
+++ scripts/cmds/robiul.js
@@ -0,0 +1,22 @@
+ module.exports = {
+ config: {
+	 name: "00robiul",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "robiul") {
+ return message.reply({
+ body: "     「রবিউল বস এখন কাজে বিজি আছেন যা বলার আমাকে বলতে পারেন🤗😚」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/SoUK0cq.mp4")
+ });
+ }
+ }
+}
